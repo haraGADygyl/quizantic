9de4b40f949e5e5b4d66c22be3982f293f9159b7
Tihomir Manushev <tihomir.manushev@genericsoft.eu>
2023-07-29 18:25:17 +0300

    Add topic logic

---
+++ database/seeders/TopicSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Topic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TopicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $topics = [
+            'PHP',
+            'Laravel',
+            'SQL',
+        ];
+
+        foreach ($topics as $topic) {
+            Topic::firstOrCreate(['name' => $topic]);
+        }
+    }
+}
