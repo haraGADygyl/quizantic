c87975ba65a0db2f5bd8175e3e1eda32730a30d0
Tihomir Manushev <tihomir.manushev@genericsoft.eu>
2023-07-30 16:47:34 +0300

    Add Breeze, refactor welcome.blade.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Level;
+use App\Models\Topic;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +11,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function home()
+    {
+        $topics = Topic::all();
+        $levels = Level::all();
+
+        return view('welcome', compact('topics', 'levels'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\TopicController;
 use Illuminate\Support\Facades\Route;
@@ -17,11 +19,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('topics', TopicController::class);
-Route::resource('questions', QuestionController::class);
-Route::resource('answers', AnswerController::class);
-Route::resource('levels', LevelController::class);
+Route::get('/', [Controller::class, 'home']);
+Route::resource('/topics', TopicController::class);
+Route::resource('/questions', QuestionController::class);
+Route::resource('/answers', AnswerController::class);
+Route::resource('/levels', LevelController::class);
+
+require __DIR__.'/auth.php';
