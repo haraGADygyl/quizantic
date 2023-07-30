0d43fb216095be50eb97e3b8790a882cf92f326e
Tihomir Manushev <tihomir.manushev@genericsoft.eu>
2023-07-30 12:07:29 +0300

    Add onDelete cascade to 'topic_id' column in 'questions' table

--- database/migrations/2023_07_29_152645_create_questions_table.php
+++ database/migrations/2023_07_29_152645_create_questions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('topic_id')->constrained();
+            $table->foreignId('topic_id')->constrained()->onDelete('cascade');
             $table->text('text');
             $table->timestamps();
         });
